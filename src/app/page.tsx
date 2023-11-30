46dd374c0ab7c97554eb8fdabee22382d469cefe
Kalleb <kallebeliasmaestri1@gmail.com>
2023-11-30 19:29:47 -0300

    colours

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-white bg-green-600">Hello Word!</h1>
+      <h1 className="text-vuejs">Hello Word</h1>
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        vuejs: {
+          100: "#49e659",
+          932: "#434252",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
