d88211715afdf241640e18dbf60b1cabb30fa098
Kalleb <kallebeliasmaestri1@gmail.com>
2023-11-30 20:08:31 -0300

    customizations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-white bg-green-600">Hello Word!</h1>
-      <h1 className="text-vuejs">Hello Word</h1>
+      <p className="text-xs sm:text-base md:text-['3px']">Hello Word</p>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,22 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    spacing: {
+      "1": "8px",
+      "2": "12px",
+      "3": "16px",
+      "4": "24px",
+      "5": "32px",
+      "6": "48px",
+    },
     extend: {
       colors: {
         vuejs: {
