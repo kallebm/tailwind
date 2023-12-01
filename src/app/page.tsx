31f2c53ab6972d7fadc45357658181a0868ef1b5
Kalleb <kallebeliasmaestri1@gmail.com>
2023-11-30 21:44:35 -0300

    typography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <div>
-      <p className="text-xs sm:text-base md:text-['3px']">Hello Word</p>
+      <h1 className="text-3xl">Title 1</h1>
+      <h2 className="text-2xl">Title 2</h2>
+      <h3 className="text-xl">Title 3</h3>
+      <p className="text-base">A regular paragraph</p>
+      <p className="text-sm">a description paragraph</p>
+      <p className="note text-xs">A little note</p>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,21 +8,11 @@ const config: Config = {
   ],
 
   theme: {
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
-    },
-    spacing: {
-      "1": "8px",
-      "2": "12px",
-      "3": "16px",
-      "4": "24px",
-      "5": "32px",
-      "6": "48px",
-    },
     extend: {
+      fontSize: {
+        sm: "15px",
+        base: "18px",
+      },
       colors: {
         vuejs: {
           100: "#49e659",
