8f48b850136e2228ae968280d84370e0888088f9
Kalleb <kallebeliasmaestri1@gmail.com>
2023-12-01 03:47:02 -0300

    space and size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">Title 1</h1>
-      <h2 className="text-2xl">Title 2</h2>
-      <h3 className="text-xl">Title 3</h3>
-      <p className="text-base">A regular paragraph</p>
-      <p className="text-sm">a description paragraph</p>
-      <p className="note text-xs">A little note</p>
+    <div className="flex w-full">
+      <div className="h-16 w-1/3 bg-blue-500 "></div>
+      <div className="h-16 w-2/3 bg-red-300"></div>
     </div>
   );
 }
